cec29d40a5ed0b22ec7ce676a329ebf4d19b6e5c
Casey MacLeod <casey@Caseys-MacBook-Pro.local>
2021-03-02 11:14:59 -0500

    adds sprites and styling

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ let $defense = $('#defense')
 let $specialAttack = $('#specialAttack')
 let $specialDefense = $('#specialDefense')
 let $speed = $('#speed')
+let $sprite = $('#sprite')
 /*----- cached element references -----*/
 /*----- event listeners -----*/
 $('form').on('submit', handleSubmit)
@@ -20,11 +21,12 @@ function handleSubmit(evt) {
     evt.preventDefault()
 
 const pokemon = $text.val()
+$text.val('')
 
     $.ajax(BASE_URL + pokemon)
     .then(function(data) {
         console.log('Data: ', data)
-        console.log(pokemon)
+        // console.log(pokemon)
         pkmnData = data
         render()
     }, function(error) {
@@ -34,11 +36,15 @@ const pokemon = $text.val()
 
 function render() {
     $pkmnName.text(pkmnData.species.name)
-    // $pkmnAbilities.text(pkmnData.abilities.ability.name)
+    // $pkmnAbilities.attr(pkmnData.abilities.ability)
     $health.text(pkmnData.stats[0].base_stat)
     $attack.text(pkmnData.stats[1].base_stat)
     $defense.text(pkmnData.stats[2].base_stat)
     $specialAttack.text(pkmnData.stats[3].base_stat)
     $specialDefense.text(pkmnData.stats[4].base_stat)
     $speed.text(pkmnData.stats[5].base_stat)
+    $sprite.attr({
+        src: pkmnData.sprites.front_default,
+        alt: pkmnData.name
+    })
 }
